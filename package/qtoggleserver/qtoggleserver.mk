c63ca9fafce245d28bab002bda64afb2c278bcc1
Calin Crisan <ccrisan@gmail.com>
2019-10-15 23:00:35 +0300

    qtoggleserver: update to 0.8.0-beta.8

--- package/qtoggleserver/qtoggleserver.mk
+++ package/qtoggleserver/qtoggleserver.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-QTOGGLESERVER_VERSION = 0.8.0-beta.7
+QTOGGLESERVER_VERSION = 0.8.0-beta.8
 QTOGGLESERVER_SOURCE = qtoggleserver-$(QTOGGLESERVER_VERSION).tar.gz
 QTOGGLESERVER_SITE = https://github.com/qtoggle/qtoggleserver/releases/download/version-$(QTOGGLESERVER_VERSION)
 QTOGGLESERVER_SETUP_TYPE = setuptools
-QTOGGLESERVER_DEPENDENCIES = host-python-fastentrypoints host-python-setupnovernormalize
 
 define QTOGGLESERVER_PATCH_VERSION
     if [ -n "$(THINGOS_VERSION)" ]; then \
+QTOGGLESERVER_DEPENDENCIES = host-python-fastentrypoints host-python-setupnovernormalize
         sed -ir "s/VERSION = .*/VERSION = '$(THINGOS_VERSION)'/" $(@D)/qtoggleserver/version.py; \
     fi
 endef
