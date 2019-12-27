fb728d45bb529cc6a74473c74690cdbb7c2e36af
Calin Crisan <ccrisan@gmail.com>
2019-12-27 02:02:57 +0200

    qtoggleserver-rpigpio: update to 1.0.1

--- package/qtoggleserver-rpigpio/qtoggleserver-rpigpio.mk
+++ package/qtoggleserver-rpigpio/qtoggleserver-rpigpio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTOGGLESERVER_RPIGPIO_VERSION = 1.0.0
+QTOGGLESERVER_RPIGPIO_VERSION = 1.0.1
 QTOGGLESERVER_RPIGPIO_SOURCE = qtoggleserver-rpigpio-$(QTOGGLESERVER_RPIGPIO_VERSION).tar.gz
 QTOGGLESERVER_RPIGPIO_SITE = https://pypi.io/packages/source/q/qtoggleserver-rpigpio
 QTOGGLESERVER_RPIGPIO_SETUP_TYPE = setuptools
