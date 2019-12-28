a5b064576a42d692ccbc9f126a67ca686351d0c6
Calin Crisan <ccrisan@gmail.com>
2019-12-28 23:31:25 +0200

    qtoggleserver: update to 0.13.0

--- package/qtoggleserver/qtoggleserver.mk
+++ package/qtoggleserver/qtoggleserver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTOGGLESERVER_VERSION = 0.13-beta.17
+QTOGGLESERVER_VERSION = 0.13.0
 QTOGGLESERVER_SOURCE = qtoggleserver-$(QTOGGLESERVER_VERSION).tar.gz
 QTOGGLESERVER_SITE = https://github.com/qtoggle/qtoggleserver/releases/download/version-$(QTOGGLESERVER_VERSION)
 QTOGGLESERVER_SETUP_TYPE = setuptools
