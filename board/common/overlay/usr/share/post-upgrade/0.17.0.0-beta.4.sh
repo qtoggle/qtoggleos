93384bd796fdedc01317d6f98bfb0e0797991d40
Calin Crisan <ccrisan@gmail.com>
2020-04-08 23:40:30 +0300

    post-upgrade/0.17.0.0-beta.4.sh: remove date setting support

--- board/common/overlay/usr/share/post-upgrade/0.17.0.0-beta.4.sh
+++ board/common/overlay/usr/share/post-upgrade/0.17.0.0-beta.4.sh
@@ -2,3 +2,7 @@
 
 # qtoggleserver.conf: fwupdate_driver -> fwupdate.driver
 sed -i 's/fwupdate_driver = "thingos.ThingOSDriver"/fwupdate = {\n        driver = "thingos.ThingOSDriver"\n    }/' /data/etc/qtoggleserver.conf
+
+# qtoggleserver.conf: remove date updating support
+awk '/date = /{c=4} c&&c--{next} 1' /data/etc/qtoggleserver.conf > /data/etc/qtoggleserver.conf.bak
+mv /data/etc/qtoggleserver.conf.bak /data/etc/qtoggleserver.conf
