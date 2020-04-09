7864c7e831b53a0dc35ac949d21f1603802537b9
Calin Crisan <ccrisan@gmail.com>
2020-04-09 21:54:14 +0300

    postupgrade/0.17.0.0-beta.4.sh: fix date set removal

--- board/common/overlay/usr/share/post-upgrade/0.17.0.0-beta.4.sh
+++ board/common/overlay/usr/share/post-upgrade/0.17.0.0-beta.4.sh
@@ -4,5 +4,5 @@
 sed -i 's/fwupdate_driver = "thingos.ThingOSDriver"/fwupdate = {\n        driver = "thingos.ThingOSDriver"\n    }/' /data/etc/qtoggleserver.conf
 
 # qtoggleserver.conf: remove date updating support
-awk '/date = /{c=4} c&&c--{next} 1' /data/etc/qtoggleserver.conf > /data/etc/qtoggleserver.conf.bak
+cat /data/etc/qtoggleserver.conf | tr '\n' '\t' | sed -r 's/date = \{[^\}]+\}\s+//' | tr '\t' '\n' > /data/etc/qtoggleserver.conf.bak
 mv /data/etc/qtoggleserver.conf.bak /data/etc/qtoggleserver.conf
