75d861a5a0c9abf665e73a367706dec7d6e938ea
Calin Crisan <ccrisan@gmail.com>
2020-05-29 01:00:24 +0300

    /usr/share/post-upgrade/0.21.0.0.sh: fix whitespace

--- board/common/overlay/usr/share/post-upgrade/0.21.0.0.sh
+++ board/common/overlay/usr/share/post-upgrade/0.21.0.0.sh
@@ -3,12 +3,12 @@
 # qtoggleserver.conf: add slaves.discover
 discover_section='    discover = { \
         enabled = true \
-        dhcp_timeout = 10 \                                                                       
-        ap = { \ 
+        dhcp_timeout = 10 \
+        ap = { \
             interface = "ap0" \
-            target_ssid = null \ 
+            target_ssid = null \
             target_psk = null \
-            finish_timeout = 300 \                                            
+            finish_timeout = 300 \
         } \
     }'
 
