3352396056c6753fe33ae227776910b6a0bc23a7
Calin Crisan <ccrisan@gmail.com>
2020-05-28 22:42:20 +0300

    qtoggleserver: update to 0.21.0-beta.1

--- package/qtoggleserver/qtoggleserver.mk
+++ package/qtoggleserver/qtoggleserver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTOGGLESERVER_VERSION = 0.20.2
+QTOGGLESERVER_VERSION = 0.21.0-beta.1
 QTOGGLESERVER_SOURCE = qtoggleserver-$(QTOGGLESERVER_VERSION).tar.gz
 QTOGGLESERVER_SITE = https://github.com/qtoggle/qtoggleserver/releases/download/version-$(QTOGGLESERVER_VERSION)
 QTOGGLESERVER_SETUP_TYPE = setuptools
