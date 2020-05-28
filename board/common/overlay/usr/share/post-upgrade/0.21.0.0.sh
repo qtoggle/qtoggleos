92dc4f2f67aeeda3ea4c66a7246ee401c64bc9aa
Calin Crisan <ccrisan@gmail.com>
2020-05-28 23:08:23 +0300

    /etc/qtoggleserver.conf: add slaves.discover section

---
+++ board/common/overlay/usr/share/post-upgrade/0.21.0.0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# qtoggleserver.conf: add slaves.discover
+discover_section='    discover = { \
+        enabled = true \
+        dhcp_timeout = 10 \                                                                       
+        ap = { \ 
+            interface = "ap0" \
+            target_ssid = null \ 
+            target_psk = null \
+            finish_timeout = 300 \                                            
+        } \
+    }'
+
+sed -ri "s/(\s+retry_count = .*)/\1\n${discover_section}/g" /data/etc/qtoggleserver.conf
