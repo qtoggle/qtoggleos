f8c5c51cf222f389b9e390e63685d6b6a76fe7c1
Calin Crisan <ccrisan@gmail.com>
2020-09-22 14:04:30 +0300

    python-qui-server: update to 1.12.1

--- package/python-qui-server/python-qui-server.mk
+++ package/python-qui-server/python-qui-server.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_QUI_SERVER_VERSION = 1.11.1
+PYTHON_QUI_SERVER_VERSION = 1.12.1
 PYTHON_QUI_SERVER_SOURCE = qui-server-$(PYTHON_QUI_SERVER_VERSION).tar.gz
-PYTHON_QUI_SERVER_SITE = https://files.pythonhosted.org/packages/a4/d3/e2e043cdc17119f24806a3f37f756493fdf195d486b620e813af6954c2d0
+PYTHON_QUI_SERVER_SITE = https://files.pythonhosted.org/packages/c9/4c/f046ee48be9cbb25227bceac1c5584f2823ead12c443f6d08add70e7e3df
 PYTHON_QUI_SERVER_SETUP_TYPE = setuptools
 PYTHON_QUI_SERVER_LICENSE = Apache-2.0
 PYTHON_QUI_SERVER_LICENSE_FILES = LICENSE.txt
