04c70178fd6914bd805d17b623182882bc5f1bc3
Calin Crisan <ccrisan@gmail.com>
2020-11-11 22:58:44 +0200

    Fix pip missing _in_process.py wrapper

--- board/common/postscript.sh
+++ board/common/postscript.sh
@@ -54,3 +54,7 @@ fi
 
 # make sure "qtoggleserver" namespace is never touched by packages
 sed -ri 's/^qtoggleserver$//g' ${TARGET}/usr/lib/python*/site-packages/*.egg-info/top_level.txt
+
+# copy uncompiled Python source required by pip
+cp ${BUILD_DIR}/python-pip-20.0.2/src/pip/_vendor/pep517/_in_process.py \
+   ${TARGET}/usr/lib/python*/site-packages/pip/_vendor/pep517
