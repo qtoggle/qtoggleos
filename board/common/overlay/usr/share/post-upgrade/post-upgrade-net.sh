91c10f8caa5b3faa3d6559f4f9c4e2244e6aa45e
Calin Crisan <ccrisan@gmail.com>
2022-11-19 15:06:25 +0200

    Add support for installing/updating/uninstalling extensions

--- board/common/overlay/usr/share/post-upgrade/post-upgrade-net.sh
+++ board/common/overlay/usr/share/post-upgrade/post-upgrade-net.sh
@@ -8,7 +8,9 @@ fi
 
 # Upgrade all qtoggleserver-* packages
 qts_packages=$(pip freeze | grep qtoggleserver- | cut -d '=' -f 1)
-
 if [[ -n "${qts_packages}" ]]; then
     pip install --upgrade $qts_packages
 fi
+
+# Update extensions to match current OS version
+extctl update
