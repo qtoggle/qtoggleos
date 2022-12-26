d67f56e31bfb2ca3db3a7ffc58ff13382ff944db
Calin Crisan <ccrisan@gmail.com>
2022-12-26 10:34:05 +0200

    python-qui-server: Bump version to 1.16.2

--- package/python-qui-server/python-qui-server.mk
+++ package/python-qui-server/python-qui-server.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_QUI_SERVER_VERSION = 1.16.1
+PYTHON_QUI_SERVER_VERSION = 1.16.2
 PYTHON_QUI_SERVER_SOURCE = qui-server-$(PYTHON_QUI_SERVER_VERSION).tar.gz
-PYTHON_QUI_SERVER_SITE = https://files.pythonhosted.org/packages/fd/ea/7e860cae6c62f18cfa7f47d6754ed6cc5755ee1b9b2d0a9246460055333d
+PYTHON_QUI_SERVER_SITE = https://pypi.io/packages/source/q/qui-server
 PYTHON_QUI_SERVER_SETUP_TYPE = setuptools
 PYTHON_QUI_SERVER_LICENSE = Apache-2.0
 PYTHON_QUI_SERVER_LICENSE_FILES = LICENSE.txt
