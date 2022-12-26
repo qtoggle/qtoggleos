829325edc380aa36de0153ca149f8d42de61d14d
Calin Crisan <ccrisan@gmail.com>
2022-12-26 12:31:29 +0200

    Update defconfigs

--- package/python-slugify/python-slugify.mk
+++ package/python-slugify/python-slugify.mk
@@ -8,5 +8,6 @@ PYTHON_SLUGIFY_VERSION = 7.0.0
 PYTHON_SLUGIFY_SOURCE = python-slugify-$(PYTHON_SLUGIFY_VERSION).tar.gz
 PYTHON_SLUGIFY_SITE = https://pypi.io/packages/source/p/python-slugify
 PYTHON_SLUGIFY_SETUP_TYPE = setuptools
+PYTHON_SLUGIFY_DEPENDENCIES = python-text-unidecode
 
 $(eval $(python-package))
