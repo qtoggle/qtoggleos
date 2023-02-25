1f91b8558d2c0c617781c18e030b9e2c1476a108
Calin Crisan <ccrisan@gmail.com>
2023-02-25 16:44:25 +0200

    qtoggleserver-pylontech: Bump version to 1.0.3

--- package/qtoggleserver-pylontech/qtoggleserver-pylontech.mk
+++ package/qtoggleserver-pylontech/qtoggleserver-pylontech.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTOGGLESERVER_PYLONTECH_VERSION = 1.0.0
+QTOGGLESERVER_PYLONTECH_VERSION = 1.0.3
 QTOGGLESERVER_PYLONTECH_SOURCE = qtoggleserver-pylontech-$(QTOGGLESERVER_PYLONTECH_VERSION).tar.gz
 QTOGGLESERVER_PYLONTECH_SITE = https://pypi.io/packages/source/q/qtoggleserver-pylontech
 QTOGGLESERVER_PYLONTECH_SETUP_TYPE = setuptools
