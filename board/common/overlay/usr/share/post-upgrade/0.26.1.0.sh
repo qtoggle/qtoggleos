88feeb1689097201984818eb55ed91a3fc510bfc
Calin Crisan <ccrisan@gmail.com>
2023-03-14 23:16:47 +0200

    qtoggleserver.conf: Bump history_janitor_interval from 600 to 3600

--- board/common/overlay/usr/share/post-upgrade/0.26.1.0.sh
+++ board/common/overlay/usr/share/post-upgrade/0.26.1.0.sh
@@ -2,3 +2,4 @@
 
 # qtoggleserver.conf: add core.device_name.get_cmd
 sed -ri 's,(set_cmd = "/usr/libexec/qs-device-name set"),get_cmd = "/usr/libexec/qs-device-name get"\n        \1,' /data/etc/qtoggleserver.conf
+sed -ri 's/history_janitor_interval = .*/history_janitor_interval = 3600/' /data/etc/qtoggleserver.conf
