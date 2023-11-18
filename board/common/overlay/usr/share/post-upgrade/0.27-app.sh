f79dd51b19a4ef965e117ff76e8a9970ac322c2c
Calin Crisan <ccrisan@gmail.com>
2023-11-18 16:27:47 +0200

    qtoggleserver: Update to 0.27.1

--- board/common/overlay/usr/share/post-upgrade/0.27-app.sh
+++ board/common/overlay/usr/share/post-upgrade/0.27-app.sh
@@ -14,6 +14,6 @@ function run_psql_cmd() {
 run_psql_cmd "\d value_history" 2>/dev/null | grep -q jsonb || exit 0
 
 run_psql_cmd "ALTER TABLE value_history RENAME TO value_history_old;"
-run_psql_cmd "SELECT id::BIGINT, TRIM((content->'pid')::TEXT, '\"') AS oid, (content->'ts')::BIGINT AS ts, CASE WHEN content->'val' = 'false' THEN 0 WHEN content->'val' = 'true' THEN 1 ELSE (content->'val')::double precision END AS val INTO value_history FROM value_history_old;"
+run_psql_cmd "SELECT id::BIGINT, TRIM((content->'pid')::TEXT, '\"') AS oid, (content->'ts')::BIGINT AS ts, CASE WHEN content->'val' = 'false' THEN 0 WHEN content->'val' = 'true' THEN 1 WHEN content->'val' = 'null' THEN 0 ELSE (content->'val')::double precision END AS val INTO value_history FROM value_history_old;"
 run_psql_cmd "ALTER TABLE value_history ALTER COLUMN id SET DEFAULT NEXTVAL('value_history_id_seq');"
 run_psql_cmd "ALTER TABLE value_history ADD PRIMARY KEY (id);"
